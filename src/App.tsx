ee0bdc9ba3eb65b80bde7db0826cc53ec992dd30
Giovanna Oliveira <150724345+GiovannaSOliveira@users.noreply.github.com>
2024-02-29 17:17:16 -0300

    Criação Home

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+
+import './App.css';
+
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+
+
+function App() {
+  return (
+    <>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
+    </>
+  );
+}
+export default App;
---
+++ src/model/UsuarioLogin.tsx
@@ -0,0 +1,9 @@
+export default interface UsuarioLogin {
+    id: number;
+    nome: string;
+    usuario: string;
+    senha: string;
+    fotous: string;
+    tipo: string;
+    token: string;
+  }
